7dbf14262b2bb4aebd32f1ae631d08efea74acf4
mohammadaminim <46426729+mohammadaminim@users.noreply.github.com>
2019-06-24 20:31:02 +0430

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args){
+		boolean continueProgramm = true;
+		
+		// :)
+		
+		while(continueProgramm){
+			switch (GUI.?()){
+				
+				case 0 : continueProgramm = false;
+			}
+			
+		}
+		
+		
+}
+}
