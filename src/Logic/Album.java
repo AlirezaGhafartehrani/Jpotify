fa4d45a961dbcf0fb3b574cbc855c48d7db7499e
mohammadaminim <46426729+mohammadaminim@users.noreply.github.com>
2019-06-26 19:57:29 +0430

    album updated

--- src/Logic/Album.java
+++ src/Logic/Album.java
@@ -1,38 +1,14 @@
+package Logic;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Album {
-    private ArrayList<Song> songs;
-    private String name;
-
+public class Album extends SongsList implements Serializable {
     Album(String name) {
         songs = new ArrayList<>();
         this.name = name;
     }
 
-    public void addToAlbum(Song song) {
-        songs.add(song);
-    }
 
-    public void removeFromAlbum(Song song) {
-        songs.remove(song);
-    }
 
-    public byte[] getAlbumImage() {
-        if (songs.isEmpty())
-            return null;
-        return songs.get(0).getImage();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean contains(Song song) {
-        return songs.contains(song);
-
-    }
-
-    public ArrayList<Song> getSongs() {
-        return songs;
-    }
 }
